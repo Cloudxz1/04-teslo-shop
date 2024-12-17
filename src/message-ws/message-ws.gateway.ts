19c839cb76346cf3c13dfa13ea36c341d51380b6
Cloudxz1 <cloudstrife1877@gmail.com>
2024-12-17 16:27:33 -0400

    avance

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,22 +1,57 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({cors: true})
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
   @WebSocketServer() wsServer: Server;
 
-  constructor(private readonly messageWsService: MessageWsService) {}
+  constructor(private readonly messageWsService: MessageWsService,
+    private readonly jwtService: JwtService
+  ) {}
   
   handleConnection(client: Socket) {
+
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try{
+      payload=this.jwtService.verify(token);
+    }catch(error){
+      client.disconnect();
+      throw new WsException('Invalid credentials')
+    }
+
+    console.log({payload});
     // console.log('Cliente conectado', client.id);
     this.messageWsService.registerClient(client);
+
     this.wsServer.emit('clients-updated',this.messageWsService.getConnectedClients());
   }
   handleDisconnect(client: Socket) {
     // console.log('Cliente desconectado', client.id);
     this.messageWsService.removeClient(client.id);
+    this.wsServer.emit('clients-updated',this.messageWsService.getConnectedClients());
+  }
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client:Socket, payload: NewMessageDto){
+    // client.emit('message-from-server', {
+    //   fullName: 'soy yo',
+    //   message:payload.message || 'no-message'
+    // })
+
+    //  client.broadcast.emit('message-from-server', {
+    //    fullName: 'soy yo',
+    //    message:payload.message || 'no-message'
+    //  })
+
+    this.wsServer.emit('message-from-server', {
+      fullName: 'soy yo',
+      message:payload.message || 'no-message'
+    })
   }
 
 
